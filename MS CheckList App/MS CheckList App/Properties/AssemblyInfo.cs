7eb9935d38fc6ad4211ceb605efdc34128948cdc
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2020-03-29 23:33:15 -0400

    update
    -Added App information
    -Added Icon

--- MS CheckList App/MS CheckList App/Properties/AssemblyInfo.cs
+++ MS CheckList App/MS CheckList App/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MS CheckList App")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MS Checklist App")]
+[assembly: AssemblyDescription("A checklist app for MapleStory")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MS CheckList App")]
+[assembly: AssemblyCompany("https://github.com/ja-me-ch")]
+[assembly: AssemblyProduct("MS Checklist App")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -52,4 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
