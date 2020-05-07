23bc2c2e82c57a0376d17476f0ba2213e1309921
ja-me-ch <60724072+ja-me-ch@users.noreply.github.com>
2020-05-07 19:21:34 -0400

    -update

--- MS CheckList App/MS CheckList App/MainWindow.xaml.cs
+++ MS CheckList App/MS CheckList App/MainWindow.xaml.cs
@@ -67,8 +67,8 @@ namespace MS_CheckList_App
 
             string initialResetDate = $"{initialDayOfWeek}, {initialDayOfMonth} {initialMonth} {initialYear} {initialTimeOfDay}";
             DateTime initialParsedDateTime = DateTime.Parse(initialResetDate);
-            
-            
+
+
             //string resetDateTime = ""
             //DateTime resetDateTime = ""
             //MessageBox.Show(currentDayOfYear);
@@ -79,6 +79,41 @@ namespace MS_CheckList_App
 
         private void MenuItem_Save_Click(object sender, RoutedEventArgs e)
         {
+            if (isProfileLoaded == true)
+            {
+                SaveExistingProfile();
+            }
+            else
+            {
+                SaveNewProfile();
+            }
+            //if (isProfileLoaded == false)
+            //{
+            //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //    saveFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            //    if (saveFileDialog.ShowDialog() == true)
+            //    {
+            //        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Profile));
+            //        safeFileName = Path.GetFileNameWithoutExtension(saveFileDialog.FileName);
+            //        StreamWriter sw = new StreamWriter(safeFileName+".xml");
+            //        profileLoaded.ProfileName = safeFileName;
+            //        xmlSerializer.Serialize(sw, profileLoaded);
+            //        sw.Close();
+            //        isProfileLoaded = true;
+            //        DataContext = null;
+            //        DataContext = profileLoaded;
+            //        //StatusBarStatusText("Profile successfully saved");
+            //        //statusbar_CurrentFile.Text = Path.GetFileName(saveFileDialog.FileName);
+            //    }
+            //    else
+            //    {
+            //        StatusBarStatusText("Save dialog exited");
+            //    }
+            //}//end of if
+        }//end of MenuItem_Save_Click
+
+        private void SaveNewProfile()
+        {
             if (isProfileLoaded == false)
             {
                 SaveFileDialog saveFileDialog = new SaveFileDialog();
@@ -87,34 +122,47 @@ namespace MS_CheckList_App
                 {
                     XmlSerializer xmlSerializer = new XmlSerializer(typeof(Profile));
                     safeFileName = Path.GetFileNameWithoutExtension(saveFileDialog.FileName);
-                    StreamWriter sw = new StreamWriter(safeFileName+".xml");
-                    profileLoaded.ProfileName = safeFileName;
+                    fileName = Path.GetFileName(saveFileDialog.FileName);
+                    StreamWriter sw = new StreamWriter(fileName);
+                    //profileLoaded.ProfileName = safeFileName;
                     xmlSerializer.Serialize(sw, profileLoaded);
                     sw.Close();
                     isProfileLoaded = true;
                     DataContext = null;
                     DataContext = profileLoaded;
-                    //StatusBarStatusText("Profile successfully saved");
-                    //statusbar_CurrentFile.Text = Path.GetFileName(saveFileDialog.FileName);
+                    statusbar_CurrentFile.Text = fileName;
+                    StatusBarStatusText("Profile successfully saved.");
+
                 }
                 else
                 {
                     StatusBarStatusText("Save dialog exited");
                 }
-            }//end of if
-            else
+            }
+        }//end of SaveNewProfile
+
+        private void SaveExistingProfile()
+        {
+            if (isProfileLoaded == true)
             {
-                StatusBarStatusText("Profile successfully saved");
                 XmlSerializer xmlSerializer = new XmlSerializer(typeof(Profile));
-                StreamWriter sw = new StreamWriter(safeFileName + ".xml");
+                //StreamWriter sw = new StreamWriter(safeFileName + ".xml");
+                StreamWriter sw = new StreamWriter(fileName);
                 xmlSerializer.Serialize(sw, profileLoaded);
                 sw.Close();
+                DataContext = null;
+                DataContext = profileLoaded;
+                StatusBarStatusText("Profile successfully saved.");
             }
-        }//end of MenuItem_Save_Click
+            else
+            {
+                StatusBarStatusText("Error saving profile.");
+            }
+            
+        }//end of SaveExistingProfile
 
         private void MenuItem_Load_Click(object sender, RoutedEventArgs e)
         {
-
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
 
@@ -123,8 +171,6 @@ namespace MS_CheckList_App
                 XmlSerializer xmlSerializer = new XmlSerializer(typeof(Profile));
                 try
                 {
-
-
                     if ((Profile)xmlSerializer.Deserialize(openFileDialog.OpenFile()) == null)
                     {
                         StatusBarStatusText("No file selected");
@@ -133,13 +179,15 @@ namespace MS_CheckList_App
                     {
                         profileLoaded.ResetAll();
                         profileLoaded = (Profile)xmlSerializer.Deserialize(openFileDialog.OpenFile());
-                        if (profileLoaded.ProfileName == null)
-                        {
-                            safeFileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
-                            profileLoaded.ProfileName = safeFileName;
-                        }
+                        //if (profileLoaded.ProfileName == null)
+                        //{
+                        //    safeFileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                        //    profileLoaded.ProfileName = safeFileName;
+                        //}
+                        safeFileName = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                        fileName = Path.GetFileName(openFileDialog.FileName);
                         StatusBarStatusText("Profile loaded successfully");
-                        statusbar_CurrentFile.Text = Path.GetFileName(openFileDialog.FileName);
+                        statusbar_CurrentFile.Text = fileName;
                         DataContext = profileLoaded;
                         isProfileLoaded = true;
                     }//end of else
@@ -158,7 +206,7 @@ namespace MS_CheckList_App
             DataContext = profileLoaded;
             StatusBarStatusText("New profile created");
         }
-
+        #region Tools -> Reset Menus
         private void MenuItem_ResetAll_Click(object sender, RoutedEventArgs e)
         {
             profileLoaded.ResetDaily();
@@ -184,15 +232,21 @@ namespace MS_CheckList_App
             profileLoaded.ResetWeeklySunday();
             statusbar_Status.Text = "Weekly (Sun) Reset complete";
         }
+        #endregion
 
         private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
         {
+            if (isProfileLoaded == true)
+            {
+                SaveExistingProfile();
+            }
             this.Close();
         }
 
+
         private void MenuItem_Settings_Click(object sender, RoutedEventArgs e)
         {
-            if ( isProfileLoaded == true)
+            if (isProfileLoaded == true)
             {
                 SettingsWindow settingsWindow = new SettingsWindow();
                 settingsWindow.Show();
@@ -203,6 +257,7 @@ namespace MS_CheckList_App
             }//end of else
         }//end of MenuItem_Settings_Click
 
+        #region About Menu
         private void MenuItem_ViewHelp_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Saving a profile will set its profile name to the filename.\n\n" +
@@ -220,7 +275,8 @@ namespace MS_CheckList_App
             {
                 MessageBox.Show("Enjoy playing Mushroom Game. uwu", "About");
             }
-        }//end of MenuItem_About_Click
+        }//end of MenuItem_About_Click 
+        #endregion
 
         public void StatusBarStatusText(string status)
         {
@@ -229,9 +285,9 @@ namespace MS_CheckList_App
 
         private Boolean EasterEggCheck(string profileName)
         {
-            List<string> cleanse = new List<string>() 
+            List<string> cleanse = new List<string>()
             {
-                "Xuey", "KunMoLee6", "Arkvaaark", "Vivin", "Stockwell", "Rakusas"
+                "Xuey", "KunMoLee6", "Arkvaaark", "Vivin", "Stockwell", "Rakusas", "Douken"
             };
             foreach (string name in cleanse)
             {
@@ -244,5 +300,181 @@ namespace MS_CheckList_App
             return false;
         }//end of EasterEggCheck
 
+        #region CheckBox_Zak_Click
+        private void chbx_eZak_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nZak.IsChecked = false;
+
+        }//end of chbx_eZak_Click
+
+        private void chbx_nZak_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eZak.IsChecked = false;
+
+        }//end of chbx_nZak_Click 
+        #endregion
+
+        #region CheckBox_VonLeon_Click
+        private void chbx_eVLeon_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nVLeon.IsChecked = false;
+            chbx_hVLeon.IsChecked = false;
+
+        }//end of chbx_eVLeon_Click
+
+        private void chbx_nVLeon_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eVLeon.IsChecked = false;
+            chbx_hVLeon.IsChecked = false;
+
+        }
+
+        private void chbx_hVLeon_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eVLeon.IsChecked = false;
+            chbx_nVLeon.IsChecked = false;
+
+        }
+
+
+        #endregion
+
+        #region CheckBox_Horntail_Click
+        private void chbx_eHTail_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nHTail.IsChecked = false;
+            chbx_cHTail.IsChecked = false;
+
+        }//chbx_eHTail_Click
+
+        private void chbx_nHTail_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eHTail.IsChecked = false;
+            chbx_cHTail.IsChecked = false;
+
+        }//end of chbx_nHTail_Click
+
+        private void chbx_cHTail_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eHTail.IsChecked = false;
+            chbx_nHTail.IsChecked = false;
+
+        }//end of chbx_cHTail_Click
+
+        #endregion
+
+        #region CheckBox_Arkarium_Click
+        private void chbx_eArk_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nArk.IsChecked = false;
+
+        }//end of chbx_eArk_Click
+
+        private void chbx_nArk_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_eArk.IsChecked = false;
+
+        }//end of chbx_nArk_Click
+        #endregion
+
+        #region CheckBox_Ranmaru_Click
+        private void chbx_nRanmaru_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_hRanmaru.IsChecked = false;
+
+        }
+
+        private void chbx_hRanmaru_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nRanmaru.IsChecked = false;
+
+        }
+        #endregion
+
+        #region CheckBox_Cygnus_Click
+        private void chbx_eCygnus_Click(object sender, RoutedEventArgs e)
+        {
+
+            chbx_nCygnus.IsChecked = false;
+
+        }
+
+        private void chbx_nCygnus_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_eCygnus.IsChecked = false;
+        }
+        #endregion
+
+        #region CheckBox_Magnus_Click
+        private void chbx_eMagnus_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_nMagnus.IsChecked = false;
+        }
+
+        private void chbx_nMagnus_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_eMagnus.IsChecked = false;
+        }
+        #endregion
+
+        #region CheckBox_Lotus_Click
+        private void chbx_nLotus_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_hLotus.IsChecked = false;
+        }
+
+        private void chbx_hLotus_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_nLotus.IsChecked = false;
+        }
+        #endregion
+
+        #region CheckBox_Damien_Click
+        private void chbx_nDamien_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_hDamien.IsChecked = false;
+        }
+
+        private void chbx_hDamien_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_nDamien.IsChecked = false;
+        }
+        #endregion
+
+        #region Check_Lucid_Click
+        private void chbx_nLucid_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_hLucid.IsChecked = false;
+        }
+
+        private void chbx_hLucid_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_nLucid.IsChecked = false;
+        }
+        #endregion
+
+        #region CheckBox_Will_Click
+        private void chbx_nWill_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_hWill.IsChecked = false;
+        }
+
+        private void chbx_hWill_Click(object sender, RoutedEventArgs e)
+        {
+            chbx_nWill.IsChecked = false;
+        }
+        #endregion
     }//end of class
 }//end of namespace
